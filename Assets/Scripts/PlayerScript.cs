74332496c63d4932cd5c8a715380873e8980a671
Menezes18 <gustavo.e.menezes@outlook.com>
2025-01-04 01:38:54 -0300

    testando movimentacao do input system

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -5,25 +5,46 @@ using UnityEngine.InputSystem;
 
 public class PlayerScript : MonoBehaviour
 {
-    [SerializeField]PlayerInput _playerInput;
-    InputAction _move;
-    InputAction _look;
-
+    [SerializeField] PlayerInput _playerInput; // input system
+    [SerializeField] CharacterController _characterController;
+    [SerializeField] private float _speed = 5.0f;
+    [SerializeField] private float _gravity = -9.81f;
+    InputAction _moveAction;
+    InputAction _lookAction;
+    
     private float _x, _y;
     float _mouseX, _mouseY;
+
+    private Vector3 _move;
+    private Transform _cam;
    void Start()
-    {
-        _move = _playerInput.actions["Move"];
-        _look = _playerInput.actions["Look"];
+    {   
+        _cam = Camera.main.transform;
+        //Pegando açao do inputsystem 
+        _moveAction = _playerInput.actions["Move"];
+        _lookAction = _playerInput.actions["Look"];
     }
 
     
     void Update()
     {
-        _x = _move.ReadValue<Vector2>().x;
-        _y = _move.ReadValue<Vector2>().y;
         
-        _mouseX = _look.ReadValue<Vector2>().x;
-        _mouseY = _look.ReadValue<Vector2>().y;
+        _x = _moveAction.ReadValue<Vector2>().x;
+        _y = _moveAction.ReadValue<Vector2>().y;
+        
+        _mouseX += _lookAction.ReadValue<Vector2>().x;
+        _mouseY += _lookAction.ReadValue<Vector2>().y;
+
+        _mouseY = Mathf.Clamp(_mouseY, -75f, 75f);  
+        
+        _move = new Vector3(_x, 0, _y);
+        _move = transform.rotation * _move;
+        _move = _move * _speed;
+        _move.y = _gravity;
+        _characterController.Move(_move * _speed * Time.deltaTime);
+        _cam.position = this.transform.position + Vector3.up * 0.5f;
+        _cam.rotation = Quaternion.Euler(new Vector3(-_mouseY, _mouseX, 0));
+        
+        transform.rotation = Quaternion.Euler(new Vector3(0, _mouseX, 0));
     }
 }
